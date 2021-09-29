3f1239a3815af451c2b12cc08ef83e8004ddb3f5
Esteban <estebanfrancobedoya@gmail.com>
2021-09-28 21:24:34 -0500

    configuracion de websocket con socket.io, introduccio de io dentro de index.html

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 const path = require('path')
+const http = require('http') // **** websocket configutation line
 const express = require('express')
-const hbs = require('hbs')
+const socketio = require('socket.io') // **** websocket configutation line
 
-const app = express()
+const app = express()  
+const server = http.createServer(app) // **** websocket configutation line, pass the server app to an http.server
+const io = socketio(server) // **** websocket configutation line, setting up socket io to work with a given server
+
 const port = process.env.PORT || 3000
-
 // define paths for express library
-const publicDirectory = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
 const partialPath = path.join(__dirname,'../templates/partial')
 
-//setup handlebars engine and views location
-app.set('view engine', 'html');
-app.engine('html', require('hbs').__express);
-app.set('views', viewsPath)  // customize views folder
-hbs.registerPartials(partialPath)
 
 //setup static directory 
-app.use(express.static(publicDirectory))
+app.use(express.static(publicDirectoryPath))
 
-// app.get('/', (req,res) =>{
-// 	res.render('index',{})
-// })
+io.on('connection',() =>{
+	console.log('new WebSocket connection!')
+})
 
-app.listen(port, () =>{
+server.listen(port, () =>{ // **** websocket configutation line
 	console.log('server is up and running on port '+port)
 })
 
